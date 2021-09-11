1f1c3e812823078c946f430235081c1a1a5726fc
apeckham <apeckham@gmail.com>
2021-09-11 11:34:52 -0700

    create a dispatcher that receives messages

--- src/nats_clj/core.clj
+++ src/nats_clj/core.clj
@@ -1,6 +1,6 @@
 (ns nats-clj.core
   (:gen-class)
-  (:import (io.nats.client Nats)
+  (:import (io.nats.client Nats MessageHandler)
            (java.nio.charset StandardCharsets)
            (java.time Duration)))
 
@@ -14,3 +14,13 @@
   (with-open [connection (Nats/connect)]
     (let [subscription (.subscribe connection "subject")]
       (prn (String. (.getData (.nextMessage subscription (Duration/ofMillis 5000))))))))
+
+(defn dispatcher
+  [& args]
+  (with-open [connection (Nats/connect)]
+    (let [dispatcher (.createDispatcher connection
+                                        (reify MessageHandler
+                                          (onMessage [_ message]
+                                            (prn (String. (.getData message))))))]
+      (.subscribe dispatcher "subject")
+      (Thread/sleep Long/MAX_VALUE))))
