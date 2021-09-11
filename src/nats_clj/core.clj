27fd2b43002094a1a1ce687b3488e7195af8b410
apeckham <apeckham@gmail.com>
2021-09-11 11:32:25 -0700

    subscribe and receive a message

--- src/nats_clj/core.clj
+++ src/nats_clj/core.clj
@@ -1,9 +1,16 @@
 (ns nats-clj.core
   (:gen-class)
   (:import (io.nats.client Nats)
-           (java.nio.charset StandardCharsets)))
+           (java.nio.charset StandardCharsets)
+           (java.time Duration)))
 
 (defn -main
   [& args]
   (with-open [connection (Nats/connect)]
     (.publish connection "subject" (.getBytes "hello world" StandardCharsets/UTF_8))))
+
+(defn incoming
+  [& args]
+  (with-open [connection (Nats/connect)]
+    (let [subscription (.subscribe connection "subject")]
+      (prn (String. (.getData (.nextMessage subscription (Duration/ofMillis 5000))))))))
